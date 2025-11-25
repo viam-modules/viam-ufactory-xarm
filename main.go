266b6524b953499eba86c5ad30b4ab61c9f7d535
Eliot Horowitz <eliothorowitz@gmail.com>
2025-11-25 17:27:36 -0500

    finish xarm, but not working

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ func main() {
 		resource.APIModel{API: arm.API, Model: xarm.XArm6Model},
 		resource.APIModel{API: arm.API, Model: xarm.XArm7Model},
 		resource.APIModel{API: arm.API, Model: xarm.XArmLite6Model},
+		resource.APIModel{API: arm.API, Model: xarm.XArm850Model},
 		resource.APIModel{API: gripper.API, Model: xarm.GripperModel},
 		resource.APIModel{API: gripper.API, Model: xarm.GripperModelLite},
 		resource.APIModel{API: gripper.API, Model: xarm.VacuumGripperModel},
