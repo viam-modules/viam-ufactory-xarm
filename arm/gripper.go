a2f958052c0daaa37b6b6991772816cc6837bb0b
Clint Purser <clint.purser@viam.com>
2026-04-01 16:56:51 -0400

    Make ‘set’ non blocking
    for vive teleop i noticed i couldn’t cancel a `set` command that was inflight in favor of a newer more desired position. This change makes set fire and forget, so that you can send in new set commands before the previous one finishes executing. the ufactory firmware handles this nicely and starts moving towards the most recent position set in.

--- arm/gripper.go
+++ arm/gripper.go
@@ -372,11 +372,9 @@ func (g *myGripper) DoCommand(ctx context.Context, cmd map[string]any) (map[stri
 	}
 	if posF, ok := cmd["set"].(float64); ok {
 		pos := int(posF)
-		_, err := g.goToPosition(ctx, pos)
-		if err != nil {
-			return nil, err
-		}
-		pos, err = g.getPosition(ctx)
+		_, err := g.arm.DoCommand(ctx, map[string]any{
+			"move_gripper": float64(pos),
+		})
 		if err != nil {
 			return nil, err
 		}
