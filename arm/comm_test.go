d6cdc900fdba4ac94049688cec549a1d512454e4
Vijay Vuyyuru <vijay.vuyyuru@viam.com>
2026-06-22 15:53:25 -0400

    Add 6-axis Force/Torque sensor resource (viam:ufactory:ft_sensor) (#83)

--- arm/comm_test.go
+++ arm/comm_test.go
@@ -1,12 +1,33 @@
 package arm
 
 import (
+	"encoding/binary"
+	"math"
 	"testing"
 
 	"go.viam.com/rdk/logging"
 	"go.viam.com/rdk/utils"
 	"go.viam.com/test"
 )
+
+func TestParseFTSensorData(t *testing.T) {
+	// Build a controller frame: 1 leading byte, then 6 little-endian float32 values.
+	want := []float64{-0.9871726, -2.9230627, -18.356257, -0.0012335, -0.0913722, 0.0069847}
+	params := make([]byte, 1+6*4)
+	for i, v := range want {
+		binary.LittleEndian.PutUint32(params[i*4+1:i*4+5], math.Float32bits(float32(v)))
+	}
+
+	got, err := parseFTSensorData(params)
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, len(got), test.ShouldEqual, 6)
+	for i := range want {
+		test.That(t, got[i], test.ShouldAlmostEqual, want[i], 1e-4)
+	}
+
+	_, err = parseFTSensorData(make([]byte, 4))
+	test.That(t, err, test.ShouldNotBeNil)
+}
 
 func TestCreateRawJointSteps1(t *testing.T) {
 	var err error
---
+++ arm/ft_sensor_test.go
@@ -0,0 +1,57 @@
+package arm
+
+import (
+	"context"
+	"testing"
+
+	"go.viam.com/rdk/components/arm"
+	"go.viam.com/test"
+)
+
+// fakeArm embeds arm.Arm so only DoCommand needs implementing.
+type fakeArm struct {
+	arm.Arm
+	lastCmd map[string]any
+	resp    map[string]any
+	err     error
+}
+
+func (f *fakeArm) DoCommand(_ context.Context, cmd map[string]any) (map[string]any, error) {
+	f.lastCmd = cmd
+	return f.resp, f.err
+}
+
+func TestFTSensorReadings(t *testing.T) {
+	fa := &fakeArm{resp: map[string]any{
+		ftSensorDataKey: map[string]any{
+			"Fx_N": -0.987, "Fy_N": -2.923, "Fz_N": -18.356,
+			"TRx_Nm": -0.0012, "TRy_Nm": -0.0914, "TRz_Nm": 0.00698,
+		},
+	}}
+	s := &ftSensor{arm: fa}
+
+	readings, err := s.Readings(context.Background(), nil)
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, readings["Fz_N"], test.ShouldEqual, -18.356)
+	test.That(t, fa.lastCmd[getFTSensorDataKey], test.ShouldEqual, true)
+}
+
+func TestFTSensorDoCommandTare(t *testing.T) {
+	fa := &fakeArm{resp: map[string]any{}}
+	s := &ftSensor{arm: fa}
+
+	_, err := s.DoCommand(context.Background(), map[string]any{tareKey: true})
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, fa.lastCmd[ftSensorZeroKey], test.ShouldEqual, true)
+}
+
+func TestFTSensorConfigValidate(t *testing.T) {
+	cfg := &FTSensorConfig{}
+	_, _, err := cfg.Validate("path")
+	test.That(t, err, test.ShouldNotBeNil)
+
+	cfg.Arm = "myarm"
+	deps, _, err := cfg.Validate("path")
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, deps, test.ShouldResemble, []string{"myarm"})
+}
